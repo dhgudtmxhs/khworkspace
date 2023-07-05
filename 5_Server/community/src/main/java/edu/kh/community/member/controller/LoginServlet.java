dfa30a84d903dbe6cc100cffcaaa7e8692c2a6a6
ohs <gudtjr1355@gmail.com>
2023-07-05 14:00:59 +0900

    커밋

--- 5_Server/community/src/main/java/edu/kh/community/member/controller/LoginServlet.java
+++ 5_Server/community/src/main/java/edu/kh/community/member/controller/LoginServlet.java
@@ -8,18 +8,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.kh.community.member.model.service.MemberService;
+import edu.kh.community.member.model.vo.Member;
+
 @WebServlet("/member/login")
 public class LoginServlet extends HttpServlet{
 
-	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		// POST 방식 요청 시 문자 인코딩이 서버 기본값으로 지정
 		// -> 한글 깨짐 -> 문자 인코딩 변경 필요
-		req.setCharacterEncoding("UTF-8");
+		//req.setCharacterEncoding("UTF-8");
 		
 		// 모든 doPost() 메소드에 인코딩 변경 코드를 작성해야함 == (귀찮음)
+		// -> 모든 요청(전달 방식 가리지 않음)시 req, resp의 문자 인코딩을 UTF-8로 변경하겠다.
+		// -> 필터(Filter)
 		
 		// 전달된 파라미터를 변수에 저장
 		String inputEmail = req.getParameter("inputEmail");
@@ -27,6 +31,28 @@ public class LoginServlet extends HttpServlet{
 		
 		System.out.println(inputEmail);
 		System.out.println(inputPw);
+		
+		
+		// 파라미터를 VO에 세팅(롬복 확인)
+		Member mem = new Member();
+		mem.setMemberEmail(inputEmail);
+		mem.setMemberPw(inputPw);
+		
+		try {
+			
+			// 서비스 객체 생성
+			MemberService service = new MemberService();
+			
+			// 이메일, 비밀번호가 일치하는 회원을 조회하는 서비스 호출 후 결과 반환받기
+			Member loginMember = service.login(mem);
+			
+			System.out.println(loginMember);
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 		
 		
 	}
--- 5_Server/community/src/main/java/edu/kh/community/member/model/dao/MemberDAO.java
+++ 5_Server/community/src/main/java/edu/kh/community/member/model/dao/MemberDAO.java
@@ -1,5 +1,87 @@
 package edu.kh.community.member.model.dao;
 
+import static edu.kh.community.common.JDBCTemplate.*;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+import edu.kh.community.member.model.vo.Member;
+
 public class MemberDAO {
+
+	private Statement stmt;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	private Properties prop;
+	
+	// Service에서 dao 객체를 생성하고 dao로 넘어옴 -> heap 메모리 안에 null값 이미 들어가있음 
+	// private Statement stmt = null 안해도 이미 null임
+	
+	
+	// 기본 생성자
+	public MemberDAO() {
+		try {
+			prop = new Properties();
+			
+			String filePath =  MemberDAO.class.getResource("/edu/kh/community/sql/member-sql.xml").getPath(); 
+			
+			prop.loadFromXML(new FileInputStream(filePath));
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+	/** 로그인 DAO
+	 * @param mem
+	 * @param conn
+	 * @return loginMember
+	 * @throws Exception
+	 */
+	public Member login(Member mem, Connection conn) throws Exception{
+	
+		Member loginMember = null; // 결과 저장용 변수
+		
+		try {
+			// SQL 얻어오기
+			String sql = prop.getProperty("login");
+			
+			// 위치홀더 쓰기 위한 PreparedStatement 생성 및 SQL 적재
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, mem.getMemberEmail());
+			
+			pstmt.setString(2, mem.getMemberPw());
+			
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				loginMember = new Member();
+				
+				loginMember.setMemberNo(         rs.getInt("MEMBER_NO")       );
+	            loginMember.setMemberEmail(    rs.getString("MEMBER_EMAIL") );
+	            loginMember.setMemberNickname(    rs.getString("MEMBER_NICK")    );
+	            loginMember.setMemberTel(       rs.getString("MEMBER_TEL")     );
+	            loginMember.setMemberAddress(    rs.getString("MEMBER_ADDR")  );
+	            loginMember.setProfileImage(    rs.getString("PROFILE_IMG")  );
+	            loginMember.setEnrollDate(       rs.getString("ENROLL_DT")     );
+			}
+			
+		}finally {
+			
+			close(rs);
+			close(pstmt);
+			
+		}
+		
+		return loginMember; // null 또는 Member 객체 주소
+	
+	}
 
 }
