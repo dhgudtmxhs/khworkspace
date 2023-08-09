046e69355f8ab22c5f528d6951e4ba7096f08ac2
OHS <gudtjr1355@gmail.com>
2023-08-09 17:34:01 +0900

    za

--- 5_Server/JSPProject/src/main/java/edu/kh/jsp/controller/ELResultServlet.java
+++ 5_Server/JSPProject/src/main/java/edu/kh/jsp/controller/ELResultServlet.java
@@ -17,6 +17,8 @@ public class ELResultServlet extends HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		//test
+		
 		// POST 방식 요청 -> 한글 깨짐 -> 문자 인코딩 처리 필요
 		req.setCharacterEncoding("UTF-8");
 		
