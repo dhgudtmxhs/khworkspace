9a85757f32f4f7a789507813cccf9d7e9cbe0842
ohs <gudtjr1355@gmail.com>
2023-11-08 10:57:38 +0900

    d

---
+++ 1_Java/05_array2/src/edu/kh/array2/ex/Main.java
@@ -0,0 +1,41 @@
+package edu.kh.array2.ex;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+    	
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	
+    	int t = Integer.parseInt(br.readLine());
+    	
+    	for(int i = 0; i < t; i++) {
+    		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    		
+    	}
+    	
+    }
+
+    	
+    	
+}
+
+
+/*
+ * Scanner sc = new Scanner(System.in);
+ * 
+ * long a = sc.nextLong();
+ * 
+ * long b = sc.nextLong();
+ * 
+ * long c = sc.nextLong();
+ * 
+ * System.out.println(a + b + c);
+ */
+
